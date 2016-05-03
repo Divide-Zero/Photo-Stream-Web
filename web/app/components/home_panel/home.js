e2c4a603fe14ac7e1d82ecdde5bc782d9968bb60
Oliver Bunyan <Dashb@me.com>
2016-05-03 22:35:54 +0100

    Transition.

--- web/app/components/home_panel/home.js
+++ web/app/components/home_panel/home.js
@@ -6,7 +6,17 @@ import Vue from 'vue'
 export default Vue.extend({
     template,
     data: () => ({
-        
+        current_slide: 0,
+        slides: [
+            {
+                message: "Hello slide 1",
+                url:"https://s3-eu-west-1.amazonaws.com/com-a2zcloud-assets/images/a2z-users/landscapes/land10.jpg",
+            },
+            {
+                message: "Hello slide 2",
+                url: "https://s3-eu-west-1.amazonaws.com/com-a2zcloud-assets/images/a2z-users/landscapes/land11.jpg",
+            },
+        ],
     }),
     computed: {
 
@@ -15,8 +25,16 @@ export default Vue.extend({
 
     },
     methods: {
-        change() {
-            this.temp = "Foo"
+        next_slide() {
+            var next = this.current_slide + 1
+            if( next > this.slides.length - 1 ) {
+                this.current_slide = 0
+                return
+            }
+            this.current_slide = next
+        },
+        is_current_slide(index) {
+            return (this.current_slide == index)
         },
     },
 })
